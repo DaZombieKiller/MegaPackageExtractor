cbde4172d09e0ed4bea23c5749aa749f944b37d5
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2022-08-03 06:00:35 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System.Text;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+
+unsafe
+{
+    if (args.Length == 0)
+    {
+        Console.Error.WriteLine("Usage: {0} <path to MegaPackage.dat> [output directory]", Path.GetFileNameWithoutExtension(Environment.ProcessPath));
+        return;
+    }
+
+    if (!File.Exists(args[0]))
+    {
+        Console.Error.WriteLine("Error: MegaPackage.dat does not exist at given location.");
+        return;
+    }
+
+    using var input = File.OpenRead(args[0]);
+    var gameRoot    = Path.GetDirectoryName(Path.GetDirectoryName(input.Name)) ?? "";
+
+    // User can override destination path instead of implied parent directory of MegaPackage.dat
+    if (args.Length > 1)
+        gameRoot = args[1];
+
+    MegaPackageHeader header;
+    input.ReadExactly(new(&header, sizeof(MegaPackageHeader)));
+
+    if (header.Magic != (('M' << 24) | ('E' << 16) | ('G' << 8) | 'A'))
+    {
+        Console.Error.WriteLine("Error: Invalid MegaPackage header.");
+        return;
+    }
+
+    var entries = new MegaPackageEntry[ReadCompactInt(input)];
+    var names   = new string[entries.Length];
+
+    for (int i = 0; i < entries.Length; i++)
+    {
+        names[i] = ReadUnString(input);
+        input.ReadExactly(MemoryMarshal.AsBytes(entries.AsSpan(i, 1)));
+    }
+
+    var blocks = new MegaPackageBlock[ReadCompactInt(input)];
+    input.ReadExactly(MemoryMarshal.AsBytes(blocks.AsSpan()));
+
+    int compressedLength;
+    input.ReadExactly(new(&compressedLength, sizeof(int)));
+
+    var origin = input.Position;
+    var data   = new byte[blocks.Length * 4096];
+
+    for (int i = 0; i < blocks.Length; i++)
+    {
+        input.Position = origin + blocks[i].Offset;
+        using var ms   = new MemoryStream(blocks[i].Length);
+        ms.SetLength(blocks[i].Length);
+        input.ReadExactly(ms.GetBuffer(), 0, blocks[i].Length);
+
+        using var ds = new ZLibStream(ms, CompressionMode.Decompress);
+        ds.ReadAtLeast(data.AsSpan(4096 * i, 4096), 4096, false);
+    }
+
+    for (int i = 0; i < entries.Length; i++)
+    {
+        var output = GetOutputDirectory(Path.GetExtension(names[i]));
+        var folder = Path.Combine(gameRoot, output);
+        Directory.CreateDirectory(folder);
+        using var fs = File.OpenWrite(Path.Combine(folder, names[i]));
+        fs.SetLength(entries[i].Length);
+        fs.Write(data, entries[i].Offset, entries[i].Length);
+    }
+}
+
+static string GetOutputDirectory(string extension)
+{
+    if (extension.EndsWith("_DFX", StringComparison.OrdinalIgnoreCase))
+        return "Sounds";
+
+    return extension.ToUpperInvariant() switch
+    {
+        ".U"   => "System",
+        ".DNF" => "Maps",
+        ".DTX" => "Textures",
+        ".DAT" => "Textures/Compiled",
+        ".DMX" => "SkinMeshes",
+        ".DFX" => "Sounds",
+        ".CVP" => "Sounds",
+        ".DSM" => "StaticMeshes",
+        ".DPS" => "Particles",
+        _      => "System",
+    };
+}
+
+static int ReadCompactInt(Stream source)
+{
+    byte b    = (byte)source.ReadByte();
+    int sign  = b & 0x80;
+    int value = b & 0x3F;
+
+    if ((b & 0x40) != 0)
+    {
+        int shift = 6;
+
+        do
+        {
+            if (shift > 27)
+                break;
+
+            b      = (byte)source.ReadByte();
+            value |= (b & 0x7F) << shift;
+            shift += 7;
+
+        } while ((b & 0x80) != 0);
+    }
+
+    return sign != 0 ? -value : value;
+}
+
+static string ReadUnString(Stream source)
+{
+    int length = ReadCompactInt(source);
+    var buffer = new byte[length];
+    source.ReadExactly(buffer);
+    var index = Array.IndexOf<byte>(buffer, 0);
+
+    if (index != -1)
+        length = index;
+
+    return Encoding.UTF8.GetString(buffer, 0, length);
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct MegaPackageHeader
+{
+    public int Magic;   // 'A', 'G', 'E', 'M'
+    public int Version; // 0
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct MegaPackageEntry
+{
+    public int Unknown0; // Always 0, version?
+    public int Offset;
+    public int Length;
+    public int Unknown1; // Flags?
+    public int Unknown2; // CRC?
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct MegaPackageBlock
+{
+    public int Offset;
+    public short Length;
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MegaPackageExtractor
- Duke Nukem Forever (2011) MegaPackage.dat extractor
+Tool to extract Duke Nukem Forever (2011)'s MegaPackage.dat format.
+
+# Building
+1. Install the [.NET 7 SDK](https://dotnet.microsoft.com/download/dotnet/7.0).
+2. Run `dotnet publish -r win-x64 -c release` in the repository root.
+3. `mpkgx.exe` will appear in `bin/Release/net7.0/win-x64/publish`.
